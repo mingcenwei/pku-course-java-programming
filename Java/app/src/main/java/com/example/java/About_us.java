ac78b07c172c8947b215fbbf9248de0d6f22af6c
sAy <47605337+mingcenwei@users.noreply.github.com>
2019-05-12 03:20:20 +0800

    Update
    Co-Authored-By: yuxiating123 <yuxiating123@users.noreply.github.com>
    Co-Authored-By: ruiyi-zhang <ruiyi-zhang@users.noreply.github.com>

---
+++ Java/app/src/main/java/com/example/java/About_us.java
@@ -0,0 +1,13 @@
+package com.example.java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class About_us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
